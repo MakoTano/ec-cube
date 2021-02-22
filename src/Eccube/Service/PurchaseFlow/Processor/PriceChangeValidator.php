8b81fe9d47cf5180856e42abdac186b45cbc9736
EC2 Default User <ec2-user@ip-172-31-43-219.us-east-2.compute.internal>
2021-02-22 16:40:34 +0000

    install Mixpack Plugin

--- src/Eccube/Service/PurchaseFlow/Processor/PriceChangeValidator.php
+++ src/Eccube/Service/PurchaseFlow/Processor/PriceChangeValidator.php
@@ -38,13 +38,15 @@ class PriceChangeValidator extends ItemValidator
         if ($item instanceof OrderItem) {
             $price = $item->getPrice();
             $realPrice = $item->getProductClass()->getPrice02();
+	    $parentItemId = $item->getParentOrderItemId();
         } else {
             // CartItem::priceは税込金額.
             $price = $item->getPrice();
             $realPrice = $item->getProductClass()->getPrice02IncTax();
+	    $parentItemId = $item->getParentCartItemId();
         }
 
-        if ($price != $realPrice) {
+        if ($price != $realPrice && is_null($parentItemId)) {
             $item->setPrice($realPrice);
             $this->throwInvalidItemException('front.shopping.price_changed', $item->getProductClass());
         }
