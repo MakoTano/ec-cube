81789cfba605a6ae94537312c07693551c98dc9d
EC2 Default User <ec2-user@ip-172-31-34-55.us-east-2.compute.internal>
2021-03-29 14:59:49 +0900

    apply b2b cart

--- src/Eccube/Service/PurchaseFlow/Processor/PriceChangeValidator.php
+++ src/Eccube/Service/PurchaseFlow/Processor/PriceChangeValidator.php
@@ -18,11 +18,43 @@ use Eccube\Entity\OrderItem;
 use Eccube\Service\PurchaseFlow\ItemValidator;
 use Eccube\Service\PurchaseFlow\PurchaseContext;
 
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+use Plugin\Mixpack\Entity\CustomerGroupMembership;
+use Plugin\Mixpack\Repository\CustomerGroupMembershipRepository;
+use Plugin\Mixpack\Entity\CustomerGroup;
+use Plugin\Mixpack\Repository\CustomerGroupRepository;
+use Plugin\Mixpack\Entity\PriceRule;
+use Plugin\Mixpack\Repository\PriceRuleRepository;
+use Plugin\Mixpack\Entity\Master\RuleType;
+use Eccube\Repository\ProductClassRepository;
+use Plugin\Mixpack\Entity\CustomerGroupProductClass;
+use Plugin\Mixpack\Repository\CustomerGroupProductClassRepository;
+
 /**
  * 販売価格の変更検知.
  */
 class PriceChangeValidator extends ItemValidator
 {
+    public function __construct(
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorizationChecker,
+        CustomerGroupMembershipRepository $customerGroupMembershipRepository,
+        CustomerGroupRepository $customerGroupRepository,
+        PriceRuleRepository $priceRuleRepository,
+        ProductClassRepository $productClassRepository,
+        CustomerGroupProductClassRepository $customerGroupProductClassRepository
+    ) {
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->customerGroupMembershipRepository = $customerGroupMembershipRepository;
+        $this->customerGroupRepository = $customerGroupRepository;
+        $this->priceRuleRepository = $priceRuleRepository;
+        $this->productClassRepository = $productClassRepository;
+        $this->customerGroupProductClassRepository = $customerGroupProductClassRepository;
+    }
+
     /**
      * @param ItemInterface $item
      * @param PurchaseContext $context
@@ -46,10 +78,79 @@ class PriceChangeValidator extends ItemValidator
 	    $parentItemId = $item->getParentCartItemId();
         }
 
+	// 会員グループID取得
+        $groupId = 1; // デフォルトグループ：ゲスト
+        if ($this->authorizationChecker->isGranted('ROLE_USER')) {
+            $Customer = $this->getUser();
+            // 会員グループID取得
+            $CustomerGroupMemberShip = $this->customerGroupMembershipRepository->find($Customer->getId());
+            if(!empty($CustomerGroupMemberShip)){
+                $groupId = $CustomerGroupMemberShip->getGroupId();
+            }
+        }
+
+        // 会員グループ別割引情報取得
+        $discountRate = 0;
+        $CustomerGroup = $this->customerGroupRepository->find($groupId);
+        if(!empty($CustomerGroup)){
+            $priceRuleId = $CustomerGroup->getPriceRuleId();
+            $PriceRule = $this->priceRuleRepository->find($priceRuleId);
+            if(!empty($PriceRule)){
+                $discountRate = $PriceRule->getRate();
+            }
+        }
+
+        // 会員グループ用金額修正
+        if($discountRate != 0){
+            $ProductClass = $this->productClassRepository->find($item->getProductClass()->getId());
+
+            $CustomerGroupProductClass = $this->customerGroupProductClassRepository->findBy(['product_class_id' => $ProductClass->getId(), 'group_id' => $groupId]);
+            if(!empty($CustomerGroupProductClass) && $CustomerGroupProductClass[0]->getPrice() > 0){
+                // グループ別商品規格価格セット
+                $newPrice = $CustomerGroupProductClass[0]->getPrice();
+            } else {
+                // グループ別割引適用
+                $productPrice = $ProductClass->getPrice02();
+                if($PriceRule->getRuleType()->getId() == RuleType::RELATIVE_DISCOUNT){
+                    $newPrice = $productPrice - ($productPrice*($discountRate/100));
+                } else {
+                    $newPrice = $productPrice - $discountRate;
+                }
+            }
+            if ($item instanceof OrderItem) {
+                if($realPrice != $newPrice){
+                    $realPrice = $newPrice;
+                    $item->setPrice($realPrice);
+                    return true;
+                }
+            } else {
+                $newPriceInTax = round($newPrice * (1+(10 / 100)));
+                if($realPrice != $newPriceInTax){
+                    $realPrice = $newPriceInTax;
+                    $item->setPrice($realPrice);
+                    return true;
+                }
+            }
+	}
+
 	// ミックスパックが有料設定の場合、選択商品を0円として登録するため
         if ($price != $realPrice  && is_null($parentItemId)) {
             $item->setPrice($realPrice);
             $this->throwInvalidItemException('front.shopping.price_changed', $item->getProductClass());
         }
+    }
+
+    protected function getUser()
+    {
+        if (null === $token = $this->tokenStorage->getToken()) {
+            return;
+        }
+
+        if (!is_object($user = $token->getUser())) {
+            // e.g. anonymous authentication
+            return;
+        }
+
+        return $user;
     }
 }
