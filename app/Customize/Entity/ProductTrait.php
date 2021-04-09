8887ac4e83b48c5982082fef2facf5ba481f63bc
belltzel <dev.belltzel@gmail.com>
2021-04-09 14:47:13 +0900

    アルコール度数の項目を追加・必要箇所へ表示を追加。CSS調整

--- app/Customize/Entity/ProductTrait.php
+++ app/Customize/Entity/ProductTrait.php
@@ -15,6 +15,13 @@ trait ProductTrait
      */
     private $publish_date;
 
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="alcohol_percentage ", type="decimal", precision=5, scale=2, nullable=true)
+     */
+    private $alcohol_percentage;
+
     public function getPublishDate(): ?\DateTimeInterface
     {
         return $this->publish_date;
@@ -25,5 +32,29 @@ trait ProductTrait
         $this->publish_date = $publish_date;
 
         return $this;
+    }
+
+    /**
+     * Set alcohol_percentage.
+     *
+     * @param string|null $alcohol_percentage
+     *
+     * @return self
+     */
+    public function setAlcoholPercentage(?string $alcohol_percentage): self
+    {
+        $this->alcohol_percentage = $alcohol_percentage;
+
+        return $this;
+    }
+
+    /**
+     * Get alcohol_percentage.
+     *
+     * @return string|null
+     */
+    public function getAlcoholPercentage(): ?string
+    {
+        return $this->alcohol_percentage;
     }
 }
