f5c3225f7b21d4ad3be2c806622ea6744d9be0b7
Bell <dev.belltzel@gmail.com>
2021-05-22 03:10:46 +0900

    会員情報に店舗名を追加

---
+++ app/Customize/Form/Extension/AdminCustomerTypeExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Customize\Form\Extension;
+
+use Eccube\Common\EccubeConfig;
+use Eccube\Form\Type\Admin\CustomerType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AdminCustomerTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * @var EccubeConfig
+     */
+    protected $eccubeConfig;
+
+    /**
+     * AdminCustomerTypeExtension constructor.
+     */
+    public function __construct(EccubeConfig $eccubeConfig)
+    {
+        $this->eccubeConfig = $eccubeConfig;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('customize_store_name', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => $this->eccubeConfig['eccube_stext_len'],
+                    ]),
+                ],
+            ]);
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function getExtendedType()
+    {
+        return CustomerType::class;
+    }
+}
