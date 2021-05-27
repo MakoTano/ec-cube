11a9d5b11dbe9bb6a321cafadbc066b09b7ecf62
EC2 Default User <ec2-user@ip-172-31-1-152.us-east-2.compute.internal>
2021-05-27 19:04:10 +0000

    Exclude Mixpack

--- src/Eccube/Service/PurchaseFlow/Processor/PriceChangeValidator.php
+++ src/Eccube/Service/PurchaseFlow/Processor/PriceChangeValidator.php
@@ -38,15 +38,13 @@ class PriceChangeValidator extends ItemValidator
         if ($item instanceof OrderItem) {
             $price = $item->getPrice();
             $realPrice = $item->getProductClass()->getPrice02();
-	    $parentItemId = $item->getParentOrderItemId();
         } else {
             // CartItem::priceは税込金額.
             $price = $item->getPrice();
             $realPrice = $item->getProductClass()->getPrice02IncTax();
-	    $parentItemId = $item->getParentCartItemId();
         }
 
-	if ($price != $realPrice && is_null($parentItemId)) {
+	if ($price != $realPrice) {
             $item->setPrice($realPrice);
             $this->throwInvalidItemException('front.shopping.price_changed', $item->getProductClass());
         }
